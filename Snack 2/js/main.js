8cb2bb652d4ab0fbd0377a6b2154713ff6e1dfbb
OgataiKhan <badellino.kevin@gmail.com>
2023-11-22 15:34:56 +0100

    feat: array of objects

---
+++ Snack 2/js/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const teams = [
+    {
+        'Name': 'Juventus',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'AC Milan',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Inter Milan',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Napoli',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Roma',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Lazio',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Fiorentina',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Atalanta',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Sassuolo',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    },
+    {
+        'Name': 'Torino',
+        'Goals Scored': 0,
+        'Fouls Suffered': 0
+    }
+];
